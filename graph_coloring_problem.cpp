a47a3aaed72d370a2739038e092c3d2c99594f24
Shardool Babasaheb Patil <shardoolpatil999@gmail.com>
2025-05-08 08:58:23 +0530

    Updated for the color changes

--- graph_coloring_problem.cpp
+++ graph_coloring_problem.cpp
@@ -1,36 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <set>
 using namespace std;
 
 class Graph {
-public:
-    int n;
     unordered_map<int, vector<int>> adjList;
 
-    Graph(int nodes) : n(nodes) {}
-
+public:
     void addEdge(int u, int v) {
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
 
     bool isSafe(int node, int color, unordered_map<int, int>& color_assignment) {
-        for (int neighbour : adjList[node]) {
-            if (color_assignment[neighbour] == color)
+        for (int neighbor : adjList[node]) {
+            if (color_assignment.count(neighbor) && color_assignment[neighbor] == color)
                 return false;
         }
         return true;
     }
 
-    bool colorGraph(int node, int maxColors, unordered_map<int, int>& color_assignment) {
-        if (node == n)
+    bool colorGraph(vector<int>& nodes, int index, int maxColors, unordered_map<int, int>& color_assignment) {
+        if (index == nodes.size())  // All nodes colored
             return true;
+
+        int node = nodes[index];
 
         for (int color = 1; color <= maxColors; color++) {
             if (isSafe(node, color, color_assignment)) {
                 color_assignment[node] = color;
-                if (colorGraph(node + 1, maxColors, color_assignment))
+                if (colorGraph(nodes, index + 1, maxColors, color_assignment))
                     return true;
-                color_assignment[node] = 0; // backtrack
+                color_assignment.erase(node); // backtrack
             }
         }
         return false;
@@ -38,21 +40,17 @@ public:
 
     void solveGraphColoring(int maxColors) {
         unordered_map<int, int> color_assignment;
-        // Initialize all nodes as unassigned (color 0)
-        for (int i = 0; i < n; i++) {
-            color_assignment[i] = 0;
-        }
-
         bool solved = false;
         int minColorsUsed = 0;
 
-        for (int colors = 1; colors <= maxColors; colors++) {
-            // Reset colors
-            for (int i = 0; i < n; i++) {
-                color_assignment[i] = 0;
-            }
+        // Collect all nodes into a list (since keys can be arbitrary)
+        vector<int> nodes;
+        for (const auto& pair : adjList)
+            nodes.push_back(pair.first);
 
-            if (colorGraph(0, colors, color_assignment)) {
+        for (int colors = 1; colors <= maxColors; colors++) {
+            color_assignment.clear();
+            if (colorGraph(nodes, 0, colors, color_assignment)) {
                 solved = true;
                 minColorsUsed = colors;
                 break;
@@ -60,9 +58,9 @@ public:
         }
 
         if (solved) {
-            cout << "\nSolution found with " << minColorsUsed << " colors:\n";
-            for (int i = 0; i < n; i++) {
-                cout << "Node " << i << " -> Color " << color_assignment[i] << endl;
+            cout << "\nSolution found with " << minColorsUsed << " colors:" << endl;
+            for (const auto& node : nodes) {
+                cout << "Node " << node << " -> Color " << color_assignment[node] << endl;
             }
 
             set<int> unique_colors;
@@ -70,34 +68,35 @@ public:
                 unique_colors.insert(entry.second);
             }
 
-            cout << "\n{Colors Used: " << unique_colors.size() << "} Used as follows:\n";
+            cout << "\n{Colors Used: " << unique_colors.size() << "} Used as follows:" << endl;
             for (int color : unique_colors) {
                 cout << "Color " << color << endl;
             }
         } else {
-            cout << "\nNo solution possible with " << maxColors << " colors.\n";
+            cout << "\nNo solution possible with " << maxColors << " colors." << endl;
         }
     }
 
     void printAdjacencyList() {
-        cout << "\nAdjacency List:\n";
+        cout << "\nAdjacency List:" << endl;
         for (const auto& pair : adjList) {
             cout << "Node " << pair.first << ": ";
-            for (int neighbour : pair.second)
-                cout << neighbour << " ";
+            for (int neighbor : pair.second) {
+                cout << neighbor << " ";
+            }
             cout << endl;
         }
     }
 };
 
 int main() {
-    int n, e, maxColors;
-    cout << "Enter number of nodes and edges: ";
-    cin >> n >> e;
+    int e, maxColors;
+    cout << "Enter number of edges: ";
+    cin >> e;
 
-    Graph g(n);
+    Graph g;
 
-    cout << "Enter edges (u v):\n";
+    cout << "Enter edges (u v):" << endl;
     for (int i = 0; i < e; i++) {
         int u, v;
         cin >> u >> v;
@@ -108,6 +107,7 @@ int main() {
     cin >> maxColors;
 
     g.printAdjacencyList();
+
     g.solveGraphColoring(maxColors);
 
     return 0;
